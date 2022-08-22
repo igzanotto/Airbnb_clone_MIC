a4340b0e064e8230a14393d4fb2640048b901505
Ignacio Zanotto <ignaciozanotto@gmail.com>
2022-08-22 18:17:02 -0300

    flats and reservations controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  
+  resources :flats do
+    resources :reservations
+  end
+
 end
