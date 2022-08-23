daf0f5a4830798343e121225c46688d19c32fbe6
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-08-23 16:59:28 -0300

    changed placeholders form new and seeds

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,13 +1,17 @@
 class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
   def index
-    # if params[:query].present?
-    #   @query = params[:query]
-    #   @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
-    #   # Preventing SQL Injection and Database error for
-    #   # unknown characters
-    # else
+    if params[:query].present?
+      @flats = Flat.where("title LIKE ?", "%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
     @flats = Flat.all
+    end
+    # if params[:query].present?
+    #   @flats = Flat.where(title: params[:query])
+    # else
+    #   @flats = Flat.all
     # end
   end
 
