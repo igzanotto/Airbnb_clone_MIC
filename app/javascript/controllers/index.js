65182272730279f2c030910ebf8d8d5f2fedb7ec
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-08-24 13:44:19 -0300

    added location input autofill to new flat form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,6 +10,9 @@ application.register("avatar", AvatarController)
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import LocationAutocompleteController from "./location_autocomplete_controller"
+application.register("location-autocomplete", LocationAutocompleteController)
+
 import MapController from "./map_controller"
 application.register("map", MapController)
 
