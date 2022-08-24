cf3705ef05e73dcdbf5476939843486e8de90653
cedric cazal <cedcaz@gmail.com>
2022-08-24 16:56:46 -0500

    modif application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home, :index]
   include Pundit::Authorization
   # after_action :verify_authorized, except: [:home, :index], unless: :skip_pundit?
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
