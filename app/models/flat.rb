e8209025c9d7c132865559aa5881ba33123a566e
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-08-25 18:53:54 -0300

    added validation photo

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,7 +5,7 @@ class Flat < ApplicationRecord
   validates :category, inclusion: { in: CATEGORIES }
   validates :description, length: { minimum: 20, maximum: 400 }
   validates :location, presence: true
-  #validates :photo, presence: true
+  validates :photo, presence: true
   has_one_attached :photo
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
